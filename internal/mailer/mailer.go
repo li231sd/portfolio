9466e446044732d4d5d766903539c57588bbb339
SS <113144462+li231sd@users.noreply.github.com>
2026-03-22 17:25:42 -0700

    new mailer

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -1,15 +1,16 @@
 package mailer
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/smtp"
 	"os"
 )
 
 func Send(name, fromEmail, message string) error {
-	gmailUser := os.Getenv("GMAIL_USER")     // your.email@gmail.com
-	gmailPass := os.Getenv("GMAIL_APP_PASS") // the app password
-	to        := os.Getenv("CONTACT_TO")     // same gmail or any email
+	gmailUser := os.Getenv("GMAIL_USER")
+	gmailPass := os.Getenv("GMAIL_APP_PASS")
+	to        := os.Getenv("CONTACT_TO")
 
 	if gmailUser == "" || gmailPass == "" {
 		fmt.Printf("\n--- Contact Form ---\nFrom: %s <%s>\n%s\n---\n", name, fromEmail, message)
@@ -17,10 +18,43 @@ func Send(name, fromEmail, message string) error {
 	}
 
 	subject := fmt.Sprintf("Portfolio contact from %s", name)
-	body := fmt.Sprintf("Name: %s\nEmail: %s\n\n%s", name, fromEmail, message)
-	msg := fmt.Sprintf("From: %s\r\nTo: %s\r\nReply-To: %s\r\nSubject: %s\r\n\r\n%s",
-		gmailUser, to, fromEmail, subject, body)
+	body    := fmt.Sprintf("Name: %s\nEmail: %s\n\n%s", name, fromEmail, message)
+	msg     := fmt.Sprintf(
+		"From: %s\r\nTo: %s\r\nReply-To: %s\r\nSubject: %s\r\n\r\n%s",
+		gmailUser, to, fromEmail, subject, body,
+	)
+
+	tlsConfig := &tls.Config{ServerName: "smtp.gmail.com"}
+	conn, err := tls.Dial("tcp", "smtp.gmail.com:465", tlsConfig)
+	if err != nil {
+		return fmt.Errorf("tls dial: %w", err)
+	}
+
+	client, err := smtp.NewClient(conn, "smtp.gmail.com")
+	if err != nil {
+		return fmt.Errorf("smtp client: %w", err)
+	}
+	defer client.Close()
 
 	auth := smtp.PlainAuth("", gmailUser, gmailPass, "smtp.gmail.com")
-	return smtp.SendMail("smtp.gmail.com:587", auth, gmailUser, []string{to}, []byte(msg))
+	if err = client.Auth(auth); err != nil {
+		return fmt.Errorf("smtp auth: %w", err)
+	}
+
+	if err = client.Mail(gmailUser); err != nil {
+		return fmt.Errorf("smtp mail: %w", err)
+	}
+	if err = client.Rcpt(to); err != nil {
+		return fmt.Errorf("smtp rcpt: %w", err)
+	}
+
+	w, err := client.Data()
+	if err != nil {
+		return fmt.Errorf("smtp data: %w", err)
+	}
+	if _, err = w.Write([]byte(msg)); err != nil {
+		return fmt.Errorf("smtp write: %w", err)
+	}
+
+	return w.Close()
 }
